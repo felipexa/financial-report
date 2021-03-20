203f7dad044c7dbcc361887c2a52242686293caa
Felipe Xavier <felipexavierass@gmail.com>
2021-03-20 16:57:40 -0300

    Project created

---
+++ financial-report/src/main/java/com/mv/financial/FinancialReportApplication.java
@@ -0,0 +1,13 @@
+package com.mv.financial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialReportApplication.class, args);
+	}
+
+}
---
+++ financial-report/src/test/java/com/mv/financial/FinancialReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mv.financial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
